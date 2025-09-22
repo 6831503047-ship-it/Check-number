e2bcc4a742f6f890de6e636bd18f3af4283112ea
Daniel Yeewangkong <jeandaniel@MacBook-Air-khxng-Daniel.local>
2025-09-22 17:06:58 +0700

    Saved

--- csharb.cs
+++ csharb.cs
@@ -1,8 +1,36 @@
-using Systeml;
-namespace P{
-    class tuty {
-        static void Main(string[]args){
-            console.WriteLine("Enter number");
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+      Console.Write ("Enter number : ");
+      int  x = Convert.ToInt32(Console.ReadLine());
+    
+        
+            Console.Write ("Enter number : ");
+            int  y = Convert.ToInt32(Console.ReadLine());
+            if (x>y){
+                Console.Write ($"{x} number greater than {y} number");
+            }else if(x<y){
+                Console.WriteLine ($"{y} number greater than {x} number");
+            }
+            else if(x==y){
+                Console.WriteLine($"{x} is equal {y}");
+            }
+            else{
+                Console.WriteLine ("Something was wrong");
+            }
+            
+            
+                
+            
         }
+        
+      
     }
-}
+
+
